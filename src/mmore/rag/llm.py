281b4cb9c471e4a2ff50e868a34e47540c8ca25d
paultltc <paul.teiletche@epfl.ch>
2024-12-17 19:08:53 +0100

    Handle endpoint-based inference
    Co-authored-by: Marc-Antoine Allard marc-antoine.allard@epfl.ch
    Co-authored-by: Thomas Kuntz thomas.kuntz@epfl.ch
    Co-authored-by: Leo Paoletti leo.paoletti@epfl.ch
    Co-authored-by: Gabriel Jimenez Calles gabriel.jimenezcalles@epfl.ch

--- src/mmore/rag/llm.py
+++ src/mmore/rag/llm.py
@@ -21,25 +21,52 @@ _ANTHROPIC_MODELS = ['claude-1', 'claude-1.3', 'claude-2', 'claude-instant-1', '
 _MISTRAL_MODELS = ['mistral-7b', 'mistral-7b-instruct', 'mistral-7b-chat']
 _COHERE_MODELS = ['command', 'command-light', 'command-nightly', 'summarize', 'embed-english-v2.0']
 
+loaders = {
+    "OPENAI": ChatOpenAI,
+    "ANTHROPIC": ChatAnthropic,
+    "MISTRAL": ChatMistralAI,
+    "COHERE": ChatCohere,
+    "HF": ChatHuggingFace
+}
 
 # TODO (@paultltc): Add generation kwargs
 @dataclass
 class LLMConfig:
     llm_name: str
+    base_url: str = None
+    organization: str = None
     max_new_tokens: int = 100
     temperature: float = 0.7
 
+    def __post_init__(self):
+        self.organization = self.organization.upper() or (
+            "OPENAI" if self.llm_name in _OPENAI_MODELS else
+            "ANTHROPIC" if self.llm_name in _ANTHROPIC_MODELS else
+            "MISTRAL" if self.llm_name in _MISTRAL_MODELS else
+            "COHERE" if self.llm_name in _COHERE_MODELS else
+            "HF" if self.base_url is None else
+            None
+        )
+
     @property
     def generation_kwargs(self):
+        max_token_key = "max_new_tokens" if (self.organization in ["ANTHROPIC", "MISTRAL", "COHERE", "HF"]) \
+                         else "max_completion_tokens"
         return {
-            "max_new_tokens": self.max_new_tokens,
             "temperature": self.temperature,
+            max_token_key: self.max_new_tokens
         }
 
-
+    @property
+    def api_key(self):
+        if self.organization:
+            LLM._check_key(self.organization)
+            return os.environ[f"{self.organization}_API_KEY"]
+        else:
+            return "EMPTY"
+    
 class LLM(BaseChatModel):
     """Class parsing the model name and arguments to load the correct LangChain model"""
-
     @staticmethod
     def _check_key(org):
         if f"{org}_API_KEY" not in os.environ:
@@ -51,23 +78,18 @@ class LLM(BaseChatModel):
         if isinstance(config, str):
             config = load_config(config, LLMConfig)
 
-        # TODO (@paultltc): [FEATURE] Handle all API based models (e.g ChatGPT) -> https://python.langchain.com/docs/how_to/qa_citations/
-        if config.llm_name in _OPENAI_MODELS:
-            LLM._check_key('OPENAI')
-            return ChatOpenAI(model=config.llm_name, temperature=config.generation_kwargs["temperature"])
-        elif config.llm_name in _ANTHROPIC_MODELS:
-            LLM._check_key('ANTHROPIC')
-            return ChatAnthropic(model=config.llm_name, **config.generation_kwargs)
-        elif config.llm_name in _MISTRAL_MODELS:
-            LLM._check_key('MISTRAL')
-            return ChatMistralAI(model=config.llm_name, **config.generation_kwargs)
-        elif config.llm_name in _COHERE_MODELS:
-            LLM._check_key('COHERE')
-            return ChatCohere(model=config.llm_name, **config.generation_kwargs)
-        else:
+        if config.organization == "HF":
             return ChatHuggingFace(llm=HuggingFacePipeline.from_model_id(
                 config.llm_name,
                 task="text-generation",
                 device_map="auto",
                 pipeline_kwargs=config.generation_kwargs
             ))
+        else:
+            loader = loaders.get(config.organization, ChatOpenAI)
+            return loader(
+                model=config.llm_name, 
+                base_url=config.base_url,
+                api_key=config.api_key,
+                **config.generation_kwargs
+            )
