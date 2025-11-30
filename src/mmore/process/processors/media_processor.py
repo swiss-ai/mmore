a9db261e23ef917a77e78000f34faad77704327d
fabnemEPFL <117652591+fabnemEPFL@users.noreply.github.com>
2025-11-30 17:59:35 +0200

    Fix create sample from processor, add page context for pdf (#188)
    Co-authored-by: Sensiel <victor.zablocki@epfl.ch>

--- src/mmore/process/processors/media_processor.py
+++ src/mmore/process/processors/media_processor.py
@@ -97,7 +97,7 @@ class MediaProcessor(Processor):
         else:
             images = []
 
-        return self.create_sample([all_text], images, file_path)
+        return self.create_sample([all_text], images, {"file_path" : file_path})
 
     def process(self, file_path: str, fast: bool = False) -> MultimodalSample:
         if not self.pipelines:
@@ -112,7 +112,7 @@ class MediaProcessor(Processor):
             if self.config.custom_config.get("extract_images", True)
             else []
         )
-        return self.create_sample([all_text], images, file_path)
+        return self.create_sample([all_text], images, {"file_path" : file_path})
 
     def _extract_text(self, file_path: str, pipeline, fast_mode=False) -> str:
         def _prepare_audio_file(file_path: str, ext: str, temp_audio):
