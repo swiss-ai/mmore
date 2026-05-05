d06626e9d7f8f696bf62eb9b242fc8199f0d8b09
JCHAVEROT <chaverotjrmy7@gmail.com>
2026-05-05 21:13:53 +0200

    chores: update imported functions
    (cherry picked from commit 8be4dc888a1774d4a1a90fc581203dc78b0e5569)

--- src/mmore/privacy/detection/__init__.py
+++ src/mmore/privacy/detection/__init__.py
@@ -1,8 +1,22 @@
+"""PII detection engines exposed as agent tools."""
+
 from .base import DetectionEngine, PIISpan
 from .config import DetectionConfig
+from .gliner_engine import GLiNEREngine, detect_pii_gliner
+from .llm_engine import LLMDetectionEngine, detect_pii_llm
+from .openai_filter_engine import OpenAIFilterEngine, detect_pii_openai
+from .presidio_engine import PresidioEngine, detect_pii_presidio
 
 __all__ = [
     "DetectionConfig",
     "DetectionEngine",
+    "GLiNEREngine",
+    "LLMDetectionEngine",
+    "OpenAIFilterEngine",
     "PIISpan",
+    "PresidioEngine",
+    "detect_pii_gliner",
+    "detect_pii_llm",
+    "detect_pii_openai",
+    "detect_pii_presidio",
 ]
